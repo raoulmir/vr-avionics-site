9fc597674efd869cadb612acc7429309b185801f
Raoul <mironraoul@gmail.com>
2023-09-23 02:50:13 +0100

    Make primary button component

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button class="rounded-md 
+  bg-gradient-to-tl hover:from-40% hover:to-90% from-maple-red-600 to-maple-red-700/80 
+  px-6 py-3 border-2 border-maple-red-400/75"
+  >
+    <slot/>
+   </button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+
+
+<style></style>
