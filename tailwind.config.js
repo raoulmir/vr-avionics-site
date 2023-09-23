0f132d9671c1e4b95912557c9ac3b54d610c5286
Raoul <mironraoul@gmail.com>
2023-09-23 02:49:57 +0100

    Project init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                'maple-red': {
+                    100: '#F0A2AC',
+                    200: '#EC8390',
+                    300: '#E76374',
+                    400: '#E24459',
+                    500: '#DD253D',
+                    600: '#D80621',
+                    700: '#BD051D',
+                    800: '#A20519',
+                    900: '#870415'
+                }
+            },
+        },
+    },
+    plugins: [],
+}
