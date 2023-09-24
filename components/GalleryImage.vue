7ea7c59f05f5640e42053b123330236f583afebc
Raoul <mironraoul@gmail.com>
2023-09-24 21:09:37 +0100

    Set min height on gallery images

--- components/GalleryImage.vue
+++ components/GalleryImage.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="group bg-cover bg-center h-72 rounded-lg border border-gray-800 drop-shadow-md relative" :style="{ 'background-image': 'url(' + thumbnailSrc + ')' }">
+<div class="group bg-cover bg-center image rounded-lg border border-gray-800 drop-shadow-md relative" :style="{ 'background-image': 'url(' + thumbnailSrc + ')' }">
     <div class="bg-black bg-opacity-30 rounded-lg h-full flex flex-col justify-center items-center">
         <div class=" container p-4 flex flex-col gap-4">
             <div class="text-container text-center">
@@ -23,4 +23,8 @@ const props = defineProps([
 
 
 
-<style></style>
+<style>
+.image {
+  min-height: 286px
+}
+</style>
