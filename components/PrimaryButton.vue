88c65714b2d81d1e656acc7a896d6bedb240ef7e
Raoul <mironraoul@gmail.com>
2023-09-24 16:40:53 +0100

    Update stying and add transition on primary button

--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <button class="rounded-md 
-  bg-gradient-to-tl hover:from-40% hover:to-90% from-maple-red-600 to-maple-red-700/80 
-  px-6 py-3 border-2 border-maple-red-400/75"
+    <button class="rounded-md transition-colors ease-in-out duration:240 bg-maple-red-600 hover:bg-maple-red-500
+  bg-gradient-to-tl from-transparent to-maple-red-800 
+  px-6 py-3 border border-maple-red-400/75"
   >
     <slot/>
    </button>
