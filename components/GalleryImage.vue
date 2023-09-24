8672900f9e5d6a767151695517b6b80239536ecf
Raoul <mironraoul@gmail.com>
2023-09-24 16:42:38 +0100

    Make gallery image component

---
+++ components/GalleryImage.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="group bg-cover bg-center h-72 rounded-lg border border-gray-800 drop-shadow-md relative" :style="{ 'background-image': 'url(' + thumbnailSrc + ')' }">
+    <div class="bg-black bg-opacity-30 rounded-lg h-full flex flex-col justify-center items-center">
+        <div class=" container p-4 flex flex-col gap-4">
+            <div class="text-container text-center">
+                <h2 class="text-white text-lg font-semibold mb-2">{{ title }}</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script setup>
+
+const props = defineProps([
+  'title',
+  'thumbnailSrc'
+])
+
+
+</script>
+
+
+
+<style></style>
