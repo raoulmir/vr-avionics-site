e6b62ef2c32f8228b33422e83246c0a462cee60b
Raoul <mironraoul@gmail.com>
2023-09-30 17:17:40 +0100

    Update package lock and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
             transitionProperty: {
                 'height': 'height'
             },
-            'display': ['Oswald', ...],
+            'display': ['Oswald']
         },
        
     },
