e8114aedee112c0f9990b60d8a5b30825c9d208d
Raoul <mironraoul@gmail.com>
2023-09-30 17:15:55 +0100

    Install and configure lite youtube embed & nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
-    '@nuxtjs/tailwindcss',
+    '@nuxt/ui',
     '@nuxtjs/google-fonts'
   ],
+
   googleFonts: {
     families: {
       Oswald: true,
     }
+  },
+
+  css: [
+    'lite-youtube-embed/src/lite-yt-embed.css'
+  ],
+
+  plugins: ['@/plugins/youtube.client.js'],
+
+  vue: {  
+    compilerOptions: {
+      isCustomElement: (tag) => ['lite-youtube'].includes(tag),
+    },
   }
 })
