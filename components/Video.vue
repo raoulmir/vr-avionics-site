c334a7600d02ca555b38f0db2946838f9b39ea17
Raoul <mironraoul@gmail.com>
2023-09-30 17:16:21 +0100

    Make video component

---
+++ components/Video.vue
@@ -0,0 +1,10 @@
+<template>
+  <lite-youtube class="h-96 w-auto" :videoid="props.videoId"
+    params="controls=1&modestbranding=1&rel=0&enablejsapi=1"></lite-youtube>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps([
+    'videoId'
+  ])
+</script>
