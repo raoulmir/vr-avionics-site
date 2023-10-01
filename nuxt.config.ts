a359c075eda836b67f967f095bc87056fab00c63
Raoul <mironraoul@gmail.com>
2023-10-01 04:49:24 +0100

    Add node-server deployment config preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'node-server'
+  },
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/ui',
     '@nuxtjs/google-fonts'
   ],
-
   googleFonts: {
     families: {
       Oswald: true,
     }
   },
-
   css: [
     'lite-youtube-embed/src/lite-yt-embed.css'
   ],
-
   plugins: ['@/plugins/youtube.client.js'],
-
   vue: {  
     compilerOptions: {
       isCustomElement: (tag) => ['lite-youtube'].includes(tag),
