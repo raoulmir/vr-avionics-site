18ad1fb2bffc99aec10bbcdc4a51962d285a2e20
Raoul <mironraoul@gmail.com>
2023-10-01 16:13:43 +0100

    Update gallery image classes on small views

--- components/GalleryImage.vue
+++ components/GalleryImage.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="group bg-cover bg-center hover:scale-105 transition-transform hover:z-40 image rounded-lg border border-gray-800 drop-shadow-md relative" :style="{ 'background-image': 'url(' + thumbnailSrc + ')' }">
-    <div class="rounded-lg h-full flex flex-col justify-center items-center" :class="title ? 'hover:bg-black/50' : ''">
+    <div class="rounded-lg h-full flex flex-col justify-center items-center" :class="title ? 'bg-black/50 md:bg-transparent md:hover:bg-black/50 ' : ''">
         <div class=" container p-4 flex flex-col gap-4">
             <div class="text-container text-center">
                 <h2 class="text-white text-lg font-semibold mb-2 md:opacity-0 md:group-hover:opacity-100">{{ title }}</h2>
