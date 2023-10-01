4059b6abe14db1fb0e95ed4cffa6a91e56dbcd6e
Raoul <mironraoul@gmail.com>
2023-10-01 04:34:21 +0100

    Update hover styles for gallery image component

--- components/GalleryImage.vue
+++ components/GalleryImage.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="group bg-cover bg-center image rounded-lg border border-gray-800 drop-shadow-md relative" :style="{ 'background-image': 'url(' + thumbnailSrc + ')' }">
-    <div class="bg-black bg-opacity-30 rounded-lg h-full flex flex-col justify-center items-center">
+<div class="group bg-cover bg-center hover:scale-105 transition-transform hover:z-40 image rounded-lg border border-gray-800 drop-shadow-md relative" :style="{ 'background-image': 'url(' + thumbnailSrc + ')' }">
+    <div class="rounded-lg h-full flex flex-col justify-center items-center" :class="title ? 'hover:bg-black/50' : ''">
         <div class=" container p-4 flex flex-col gap-4">
             <div class="text-container text-center">
-                <h2 class="text-white text-lg font-semibold mb-2">{{ title }}</h2>
+                <h2 class="text-white text-lg font-semibold mb-2 md:opacity-0 md:group-hover:opacity-100">{{ title }}</h2>
             </div>
         </div>
     </div>
