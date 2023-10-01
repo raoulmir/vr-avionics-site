56b3437cfe09f07cd7f599697cd690420007341c
Raoul <mironraoul@gmail.com>
2023-10-01 04:34:47 +0100

    Update pimary button styling and apply functionality

--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <button class="rounded-md transition-colors ease-in-out duration:240 bg-maple-red-600 hover:bg-maple-red-500
-  bg-gradient-to-tl from-transparent to-maple-red-800 
+    <NuxtLink :to="props.to" target="_blank" class="cursor-pointer rounded-md transition-colors ease-in-out duration:240 bg-maple-red-600 hover:bg-maple-red-500
+  bg-gradient-to-tl from-transparent ring-2 ring-maple-red-600 to-maple-red-800 font-display font-semibold
   px-6 py-3 border border-maple-red-400/75"
   >
     <slot/>
-   </button>
-  </div>
+  </NuxtLink>
 </template>
 
 <script setup>
-
+const props = defineProps([
+  'to'
+])
 </script>
 
 
