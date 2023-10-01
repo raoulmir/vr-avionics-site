8e48fe4b3f911b16b30b9b1cdf2b470630dad22e
Raoul <mironraoul@gmail.com>
2023-10-01 04:34:59 +0100

    Create secondary button component

---
+++ components/SecondaryButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <NuxtLink :to="props.to" target="_blank" class="cursor-pointer rounded-md transition-colors ease-in-out duration:240 bg-slate-600 hover:bg-slate-500
+  bg-gradient-to-tl from-transparent to-slate-800 font-semibold ring-2 ring-slate-800
+  px-6 py-3 border border-slate-700/75 font-display"
+  >
+    <slot/>
+  </NuxtLink>
+</template>
+
+<script setup>
+const props = defineProps([
+  'to'
+])
+</script>
+
+
+
+<style></style>
